ccae7ef13fd103eeafe581c002325fc655e074a6
Gazynova <somnath.prajapati@neosoftmail.com>
2025-07-21 19:25:28 +0530

    updated

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { routes } from '../app.routes';
-<<<<<<< HEAD
-import { FooterComponent } from "../shared/footer/footer.component";
-import { HeaderComponent } from "../shared/header/header.component";
-import { CarouselComponent } from "../shared/carousel/carousel.component";
-import { SkillsCarouselComponent } from "../shared/skills-carousel/skills-carousel.component";
-@Component({
-  selector: 'app-homepage',
-  imports: [FooterComponent, HeaderComponent, CarouselComponent, SkillsCarouselComponent],
-=======
 import { LogoCarouselComponent } from "../logo-carousel/logo-carousel.component";
 
 @Component({
   selector: 'app-homepage',
   imports: [LogoCarouselComponent],
->>>>>>> 816dc74aa4b08c4204a827b9a5155e8d7c1cd42e
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })

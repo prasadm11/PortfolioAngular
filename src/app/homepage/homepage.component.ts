596f320715fbac56762bb602d5937031c7fbd3b0
Gazynova <somnath.prajapati@neosoftmail.com>
2025-07-21 15:34:00 +0530

    created the shared components

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { routes } from '../app.routes';
+import { FooterComponent } from "../shared/footer/footer.component";
+import { HeaderComponent } from "../shared/header/header.component";
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })

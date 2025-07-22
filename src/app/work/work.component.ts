001bd934ea1aca3cadaff55646b449d511172a64
prasadm11 <pmmahajan2002@gmail.com>
2025-07-22 13:15:10 +0530

    Added some improvements in workcompoenent

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -13,19 +13,22 @@ export class WorkComponent {
     meta: 'AIRBNB · 2023',
     title: 'Curating AR experiences while travelling',
     points: ['Onboarding increased to 12%', 'New user signups increased by 32%', 'Engagement increased by 20%'],
-    image: '/assets/ar-preview.png'
+    image: '/assets/ar-preview.png',
+    link: '/case-study/ar'
   },
   {
     meta: 'GOOGLE · 2022',
     title: 'Optimizing UX for Search Console',
     points: ['Bounce rate reduced by 15%', 'SEO score improved', 'Increased dashboard usage'],
-    image: '/assets/google-preview.png'
+    image: '/assets/google-preview.png',
+    link: '/case-study/ar'
   },
   {
     meta: 'NIKE · 2021',
     title: 'Immersive E-Commerce Experience',
     points: ['Conversion improved by 27%', 'Faster page loads', 'High user retention'],
-    image: '/assets/nike-preview.png'
+    image: '/assets/nike-preview.png',
+    link: '/case-study/ar'
   }
 ];
 

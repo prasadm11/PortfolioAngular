c4cd04cd851a7f9a1b895eacc80d488b871d845f
prasadm11 <pmmahajan2002@gmail.com>
2025-07-23 12:51:13 +0530

    added about componenet

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component ,HostListener} from '@angular/core';
+import { HeaderComponent } from "../shared/header/header.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  rotation = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY;
+    this.rotation = scrollY * 0.9; // control speed here
+
+    const icon = document.querySelector('.pixel-icon') as HTMLElement;
+    if (icon) {
+      icon.style.transform = `rotate(${this.rotation}deg)`;
+    }
+  }
 
 }

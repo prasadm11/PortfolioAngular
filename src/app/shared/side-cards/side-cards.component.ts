a95560f6345a5582eedfac3670bd12fc2e9c16ef
prasadm11 <pmmahajan2002@gmail.com>
2025-07-27 14:55:20 +0530

    photos

--- src/app/shared/side-cards/side-cards.component.ts
+++ src/app/shared/side-cards/side-cards.component.ts
@@ -5,41 +5,45 @@ import { Component } from '@angular/core';
   selector: 'app-side-cards',
   imports: [CommonModule],
   templateUrl: './side-cards.component.html',
-  styleUrl: './side-cards.component.css'
+  styleUrl: './side-cards.component.css',
 })
 export class SideCardsComponent {
   hoveredIndex: number | null = null;
 
   cards = [
-    { image: 'https://images.pexels.com/photos/1697912/pexels-photo-1697912.jpeg' },
-    { image: 'https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg' },
-    { image: 'https://images.pexels.com/photos/1848565/pexels-photo-1848565.jpeg' },
-    { image: 'https://images.pexels.com/photos/1019771/pexels-photo-1019771.jpeg' },
-    { image: 'https://images.pexels.com/photos/13374150/pexels-photo-13374150.jpeg' }
+    // { image: 'https://images.pexels.com/photos/1697912/pexels-photo-1697912.jpeg' },
+    // { image: 'https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg' },
+    // { image: 'https://images.pexels.com/photos/1848565/pexels-photo-1848565.jpeg' },
+    // { image: 'https://images.pexels.com/photos/1019771/pexels-photo-1019771.jpeg' },
+    // { image: 'https://images.pexels.com/photos/13374150/pexels-photo-13374150.jpeg' },
+    { image: 'assets/img/sidecard/img/img4.jpg' },
+    { image: 'assets/img/sidecard/img/img2.jpg' },
+    { image: 'assets/img/sidecard/img/img3.jpg' },
+    { image: 'assets/img/sidecard/img/img1 (1).jpg' },
+    { image: 'assets/img/sidecard/img/img5.jpg' },
   ];
 
+  getCardTransform(index: number): string {
+    const overlap = 90; // horizontal shift between cards
+    const rotate = (index - (this.cards.length - 1) / 2) * 4; // rotation for fan-like layout
 
-getCardTransform(index: number): string {
-  const overlap = 90; // horizontal shift between cards
-  const rotate = (index - (this.cards.length - 1) / 2) * 4; // rotation for fan-like layout
+    if (this.hoveredIndex === null) {
+      const translateX = index * overlap;
+      return `translateX(${translateX}px) rotate(${rotate}deg)`;
+    }
 
-  if (this.hoveredIndex === null) {
-    const translateX = index * overlap;
-    return `translateX(${translateX}px) rotate(${rotate}deg)`;
+    if (index === this.hoveredIndex) {
+      return `translateY(-20px) scale(1.1) translateX(${
+        index * overlap
+      }px) rotate(${rotate}deg)`;
+    }
+
+    const direction = index < this.hoveredIndex ? -1 : 1;
+    const shift = 40; // how far non-hovered cards move away
+    const translateX = index * overlap + direction * shift;
+
+    return `translateX(${translateX}px) scale(0.95) rotate(${rotate}deg)`;
   }
-
-  if (index === this.hoveredIndex) {
-    return `translateY(-20px) scale(1.1) translateX(${index * overlap}px) rotate(${rotate}deg)`;
-  }
-
-  const direction = index < this.hoveredIndex ? -1 : 1;
-  const shift = 40; // how far non-hovered cards move away
-  const translateX = index * overlap + direction * shift;
-
-  return `translateX(${translateX}px) scale(0.95) rotate(${rotate}deg)`;
-}
-
-
 
   handleMouseMove(event: MouseEvent, index: number): void {
     const card = event.currentTarget as HTMLElement;
@@ -56,6 +60,5 @@ getCardTransform(index: number): string {
     const card = cards[index] as HTMLElement;
     card.style.transform = this.getCardTransform(index);
     this.hoveredIndex = null;
-  
   }
 }
